32bcd7464c3e46d44b6e0c649becaf6ea54b0629
Alex Pohlman <alexpohlman@Alexs-MacBook-Pro.local>
2023-10-29 19:35:09 -0400

    added getData + writeData functions and created dbPath variable for them

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 3333;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const dbPath = path.join(__dirname, "db.json");
+
 // GET /notes should return the notes.html file.
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/notes.html"), function (err) {
@@ -35,15 +37,8 @@ app.get("*", function (req, res) {
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 app.get("/api/notes", function (req, res) {
-  fs.readFile("db.json", "utf-8", function (err, data) {
-    if (err) {
-      console.error("Error reading file:", err);
-      res.status(500).send("Internal Server Error");
-    } else {
-      const notes = JSON.parse(data);
-      res.json(notes);
-    }
-  });
+  const notes = getData();
+  res.json(notes);
 });
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
@@ -51,67 +46,53 @@ app.post("/api/notes", function (req, res) {
   const newNote = req.body;
   const newNoteId = generateUniqueId();
 
-  // Read the contents of the db.json file
-  fs.readFile("db.json", "utf-8", function (err, data) {
-    if (err) {
-      console.error("Error reading file:", err);
-      res.status(500).send("Internal Server Error");
-    } else {
-      const notes = JSON.parse(data);
+  const notes = getData();
+  newNote.id = newNoteId;
+  notes.push(newNote);
 
-      newNote.id = newNoteId;
-      notes.push(newNote);
+  writeData(notes);
 
-      // Write the updated notes data back to the db.json file
-      fs.writeFile("db.json", JSON.stringify(notes), function (err) {
-        if (err) {
-          console.error("Error writing file:", err);
-          res.status(500).send("Internal Server Error");
-        } else {
-          res.json(newNote);
-        }
-      });
-    }
-  });
+  res.json(newNote);
 });
 
 // DELETE notes by ID
 app.delete("/api/notes/:id", function (req, res) {
   const noteId = req.params.id;
 
-  // Read the JSON file
-  fs.readFile("db.json", "utf-8", function (err, data) {
-    if (err) {
-      console.error("Error reading file:", err);
-      res.status(500).send("Internal server error");
-    } else {
-      const notes = JSON.parse(data);
+  const notes = getData();
+  const filteredNotes = notes.filter((note) => note.id !== noteId);
 
-      // Find notes with the matching ID
-      const filteredNotes = notes.filter((note) => note.id !== noteId);
+  if (filteredNotes.length === notes.length) {
+    res.status(404).send("Note not found");
+    return;
+  }
 
-      // Checks if a note was deleted
-      if (filteredNotes.length === notes.length) {
-        res.status(404).send("Note not found");
-        return;
-      }
+  writeData(filteredNotes);
 
-      // Write the updated notes data in the db.json
-      fs.writeFile("db.json", JSON.stringify(filteredNotes), function (err) {
-        if (err) {
-          console.error("Error writing file:", err);
-          res.status(500).send("Internal server error");
-        } else {
-          res.status(200).send("Note deleted!");
-        }
-      });
-    }
-  });
+  res.status(200).send("Note deleted!");
 });
 
 // Function to generate unique ID for each note
 function generateUniqueId() {
-  return uuid.v4();
+  return uuidv4();
+}
+
+// Retrieves the data and returns as an object
+function getData() {
+  const json = fs.readFileSync(dbPath, "utf-8");
+  return JSON.parse(json);
+}
+
+// Take in the new database array and stringify it, then overwrite the old db.json with the new array
+function writeData(dbArray) {
+  fs.writeFile(dbPath, JSON.stringify(dbArray, null, 2), (err) => {
+    if (err) {
+      console.error("Error writing file:", err);
+      res.status(500).send("Internal server error");
+    } else {
+      console.log("Database Updated Successfully");
+    }
+  });
 }
 
 app.listen(PORT, function () {
