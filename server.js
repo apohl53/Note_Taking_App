ed5f51dd207e43710a8ebb1d7fafc3dd4a43f4ad
Alex Pohlman <alexpohlman@Alexs-MacBook-Pro.local>
2023-10-31 20:56:48 -0400

    reordered routes to fix display issue

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const express = require("express");
-const { v4: uuidv4 } = require("uuid");
 const { getData, writeData, generateId } = require("./db");
 const path = require("path");
 
@@ -11,6 +10,50 @@ app.use(express.static("./public"));
 app.use(express.json());
 
 const dbPath = path.join(__dirname, "db.json");
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get("/api/notes", function (req, res) {
+  getData()
+    .then((notes) => {
+      res.json(notes);
+    })
+    .catch((err) => {
+      console.error("Error getting data:", err);
+      res.status(500).send("Internal Server Error");
+    });
+});
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+app.post("/api/notes", function (req, res) {
+  const noteData = getData();
+
+  const newNote = {
+    id: generateId(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+
+  noteData.push(newNote);
+  writeData(noteData);
+
+  res.json({ message: "Database Updated" });
+});
+
+// DELETE notes by ID
+app.delete("/api/notes/:id", function (req, res) {
+  const noteId = req.params.noteId;
+  const notes = getData();
+
+  const noteIndex = notes.findIndex((note) => note.id === noteId);
+
+  if (noteIndex !== -1) {
+    notes.splice(noteIndex, 1);
+    writeData(notes);
+    res.json({ message: "Database Updated" });
+  } else {
+    res.json({ error: "Note was not found" });
+  }
+});
 
 // GET /notes should return the notes.html file.
 app.get("/notes", function (req, res) {
@@ -34,47 +77,6 @@ app.get("*", function (req, res) {
       console.log("File sent successfully");
     }
   });
-});
-
-// GET /api/notes should read the db.json file and return all saved notes as JSON.
-app.get("/api/notes", function (req, res) {
-  const notes = getData();
-  res.json(notes);
-});
-
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-app.post("/api/notes", function (req, res) {
-  const noteData = getData();
-
-  const newNote = {
-    id: generateId(),
-    title: req.body.title,
-    text: req.body.text,
-  };
-
-  noteData.push(newNote);
-
-  writeData(noteData);
-
-  res.json({ message: "Database Updated" });
-});
-
-// DELETE notes by ID
-app.delete("/api/notes/:id", function (req, res) {
-  const noteId = req.params.noteId;
-  const notes = getData();
-
-  const noteIndex = notes.findIndex((note) => note.id === noteId);
-
-  if (noteIndex !== -1) {
-    notes.splice(noteIndex, 1);
-
-    writeData(notes);
-
-    res.json({ message: "Database Updated" });
-  } else {
-    res.json({ error: "Note was not found" });
-  }
 });
 
 app.listen(PORT, function () {
